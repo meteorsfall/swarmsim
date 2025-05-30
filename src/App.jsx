2b85946639ffc135ebd9a8509d8c3563f1ffa790
Louise Song <louise.s.song@gmail.com>
2025-05-29 17:34:28 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+import {useDispatch} from 'react-redux';
+import Header from './Header';
+
+export default function App() {
+	return (
+		<div>
+			<Header />
+		</div>
+	)
+}
